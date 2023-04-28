e56294048151c9c10c74065609d955936d31dd20
Olena-Rudnieva <rudnievao@gmail.com>
2023-04-28 17:58:02 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,8 @@ console.log('Number of categories:', idEl.children.length);
 const categorieTitle = idEl.querySelectorAll('h2');
 
 categorieTitle.forEach((element) => {
-    console.log('Category:', element.textContent);
-    console.log('Elements', categorieTitle.length);
+  console.log('Category:', element.textContent);
+  const listEl = element.nextElementSibling;
+  const listItemEl = listEl.children;
+  console.log('Elements', listItemEl.length);
 });
-
