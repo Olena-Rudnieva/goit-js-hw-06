2010713c5d7b6b9ee48aa2a9b7d4bafe20780d61
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-01 11:16:36 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,8 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl = document.querySelector('li');
+// itemEl.textContent = 'Potatoes';
+// itemEl.classList.add('item');
+console.log(itemEl);
