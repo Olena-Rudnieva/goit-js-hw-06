0d7f0b98267155419de9e349640ccfecd198c4ea
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-03 17:12:56 +0300

    Update task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const counterValue = {
+  value: 0,
+  increment() {
+    this.value += 1;
+  },
+  decrement() {
+    this.value -= 1;
+  },
+};
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+incrementBtn.addEventListener('click', function () {
+  counterValue.increment(), (valueEl.textContent = counterValue.value);
+});
+
+decrementBtn.addEventListener('click', function () {
+  counterValue.decrement(), (valueEl.textContent = counterValue.value);
+});
