ce08be1a3e4bc713e163953ed56931b0bd8cb1b3
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-04 00:03:08 +0300

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector('#validation-input');
+
+function onInputBlur(event) {
+  if (
+    event.currentTarget.value.length ===
+    Number(inputEl.getAttribute('data-length'))
+  ) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
+  }
+}
+
+inputEl.addEventListener('blur', onInputBlur);
