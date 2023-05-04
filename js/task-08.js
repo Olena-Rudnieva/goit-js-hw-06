663d02fd7bf7f99c415bb3f706866f5acea1d8dc
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-04 17:38:32 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form');
+console.log(form);
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+
+  if (email === '' || password === '') {
+    const message = 'Заповніть це поле!';
+    alert(message);
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+  }
+  event.currentTarget.reset();
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onClearForm() {
+  console.log(email);
+}
