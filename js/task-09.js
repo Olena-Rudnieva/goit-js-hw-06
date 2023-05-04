70d9c6d25bf08b98f563ce15135e723d4d5ebe4a
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-04 18:43:15 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+const body = document.body;
+
+function onColorChange() {
+  let newColor = getRandomHexColor();
+  body.style.background = newColor;
+  colorEl.textContent = newColor;
+}
+
+btnEl.addEventListener('click', onColorChange);
