0456b2b52ba58e22bac9206f15998831505983d3
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-05 21:53:49 +0300

    Final update

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const inputEl = document.querySelector('#font-size-control');
 const textEl = document.querySelector('#text');
 
 function onInputRange(event) {
-    console.log(event.currentTarget.value);
+  console.log(event.currentTarget.value);
   textEl.style.fontSize = `${event.currentTarget.value}px`;
 }
 
