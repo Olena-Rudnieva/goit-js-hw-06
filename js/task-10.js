d8786c485fed362f86464c11a3f45129643fd262
Olena-Rudnieva <rudnievao@gmail.com>
2023-05-05 21:46:21 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,46 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inputEl = document.querySelector('#controls input');
+console.log(inputEl);
+const btnCreate = document.querySelector('button[data-create]');
+console.log(btnCreate);
+const btnDestroy = document.querySelector('button[data-destroy]');
+console.log(btnDestroy);
+const boxEl = document.querySelector('#boxes');
+console.log(boxEl);
+
+function onInputChange() {
+  let boxAmount = Number(inputEl.value);
+  return boxAmount;
+}
+
+function onCreateBoxes() {
+  createBoxes(onInputChange());
+}
+
+function createBoxes(amount) {
+  let boxArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    let sizes = 30 + 10 * i;
+    const newBox = document.createElement('div');
+    newBox.style.width = `${sizes}px`;
+    newBox.style.height = `${sizes}px`;
+    let newColor = getRandomHexColor();
+    newBox.style.background = newColor;
+    boxArray.push(newBox);
+  }
+  return boxEl.append(...boxArray);
+}
+
+function destroyBoxes() {
+  inputEl.value = '';
+  return (boxEl.innerHTML = '');
+}
+
+inputEl.addEventListener('input', onInputChange);
+
+btnCreate.addEventListener('click', onCreateBoxes);
+
+btnDestroy.addEventListener('click', destroyBoxes);
